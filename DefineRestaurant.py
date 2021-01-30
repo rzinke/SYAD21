685c387f414bf4265f6e415746a775ae66fb13dc
Morgan <55892175+morganpothoff@users.noreply.github.com>
2021-01-30 17:00:13 -0600

    Completes Restaurant object

--- DefineRestaurant.py
+++ DefineRestaurant.py
@@ -1,21 +1,31 @@
-#!/usr/bin python3
-'''
-Define restaraunt bounds.
-'''
-### MODULES ---
-from shapely.geometry import LineString
-from matplotlib.pyplot import plt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = "morganpothoff"
 
+########################################################################################################################
+#                                                                                                                      #
+#   created by: morganpothoff                                                                                          #
+#   on 2021.01.30                                                                                                      #
+#                                                                                                                      #
+#   DESCRIPTION: Morgan is the worst.                                                                                  #
+#   BUGS:                                                                                                              #
+#   FUTURE:                                                                                                            #
+#                                                                                                                      #
+########################################################################################################################
+
+
+from shapely.geometry import LineString
+import matplotlib.pyplot
 
 
 ### CLASSES ---
-class restaurant:
+class Restaurant:
     def __init__(self, vertices):
         '''
-        Define the bounds of a restaurant based on the corners of the room(s).
+        Define the bounds of a restaurant based on the vertices of the room(s).
         '''
         # Define walls
-        self.walls = LineString(corners)
+        self.walls = LineString(vertices)
 
 
     def plot(self):
@@ -24,3 +34,23 @@ class restaurant:
         '''
         # Spawn figure
         fig, ax = plt.subplots()
+
+
+# Reads points from file.
+# Takes filename.
+# Uses space-separated-variables (ssv). Reads from file. Separate x-y coordinates.
+#  Convert points to list of ints.
+# Returns array of points (x, y).
+def filePoints(filename):
+	with open(filename, "r") as file:
+		return [[int(coord) for coord in line.rstrip().split(" ")] for line in file.readlines()]
+
+
+def main():
+	vertices = filePoints("points.txt")
+	restaurant = Restaurant(vertices)
+	print(restaurant.walls)
+
+
+if __name__ == '__main__':
+	main()
